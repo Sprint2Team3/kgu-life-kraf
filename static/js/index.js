0163fa2cd2369b706632f55d543d8a242dafcd6b
intellij-Laptop <ysh062100@gmail.com>
2025-02-13 12:55:55 +0900

    fix[유저] : 회원가입 안되는 오류 수정

--- static/js/index.js
+++ static/js/index.js
@@ -56,67 +56,42 @@ window.sendEmail = function () {
   }
 };
 // 인증번호 확인
-window.checkAuth = function () {
-  const verificationCode = document
-    .getElementById("verification-code")
-    .value.trim();
-  const verificationError = document.getElementById("verification-error");
-  const verificationCheckIcon = document.getElementById(
-    "verification-check-icon"
-  );
-  const signupBtn = document.getElementById("signup-btn");
+window.checkAuth = function() {
+    const verificationCode = document.getElementById('verification-code').value.trim();
+    const verificationError = document.getElementById('verification-error');
+    const verificationCheckIcon = document.getElementById('verification-check-icon');
+    const signupBtn = document.getElementById('signup-btn');
+    email = $('#email-input').val();
 
-  // AJAX로 이메일 중복 확인 요청
-  $.ajax({
-    url: "/check-auth",
-    type: "POST",
-    contentType: "application/json",
-    data: JSON.stringify({ authCode: verificationCode }),
-    success: function (response) {
-      if (response.success) {
-        alert("인증번호가 확인되었습니다.");
-        verificationError.classList.add("d-none"); // 오류 메시지 숨김
-        verificationCheckIcon.classList.remove("d-none"); // 체크 아이콘 표시
-        isAuth = true;
-        signupBtn.disabled = false;
-      } else {
-        verificationError.classList.remove("d-none"); // 오류 메시지 표시
-        verificationCheckIcon.classList.add("d-none"); // 체크 아이콘 숨김
-        isAuth = false;
-        signupBtn.disabled = true;
-      }
-    },
-    error: function () {
-      alert("인증과정에서 오류가 발생하였습니다.");
-    },
-  });
-};
-// 인증번호 확인
-window.checkAuth = function () {
-  verificationCode = document.getElementById("verification-code").value.trim();
-  const verificationError = document.getElementById("verification-error");
-  const verificationCheckIcon = document.getElementById(
-    "verification-check-icon"
-  );
-  const signupBtn = document.getElementById("signup-btn");
 
-  $.ajax({
-    url: "/check-auth",
-    type: "POST",
-    contentType: "application/json",
-    data: JSON.stringify({ authCode: verificationCode }),
-    success: function (response) {
-      verificationError.classList.add("d-none"); // 오류 메시지 숨김
-      verificationCheckIcon.classList.remove("d-none"); // 체크 아이콘 표시
-      isAuth = true;
-    },
-    error: function () {
-      verificationError.classList.remove("d-none"); // 오류 메시지 표시
-      verificationCheckIcon.classList.add("d-none"); // 체크 아이콘 숨김
-      isAuth = false;
-    },
-  });
-};
+    $.ajax({
+        url: '/check-auth',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify({ email: email, authCode: verificationCode }),
+        success: function (response) {
+            if(response.success) {
+                alert("인증번호가 확인되었습니다.")
+                verificationError.classList.add('d-none'); // 오류 메시지 숨김
+                verificationCheckIcon.classList.remove('d-none'); // 체크 아이콘 표시
+                isAuth = true;
+                signupBtn.disabled = false;
+                $('#email-input').prop("disabled", true);
+                $('#send-code-btn').prop("disabled", true);
+                $('#verification-code').prop("disabled", true);
+                $('#verify-code-btn').prop("disabled", true);
+            } else {
+                verificationError.classList.remove('d-none'); // 오류 메시지 표시
+                verificationCheckIcon.classList.add('d-none'); // 체크 아이콘 숨김
+                isAuth = false;
+                signupBtn.disabled = true;
+            }
+        },
+        error: function () {
+            alert("인증과정에서 오류가 발생하였습니다.")
+        }
+    });
+}
 
 //회원가입
 window.checkSignup = function () {
